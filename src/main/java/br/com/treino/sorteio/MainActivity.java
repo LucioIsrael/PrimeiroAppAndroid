42471a2edd0eaf91b4e3f29ce2292d7eb27f3f49
Lúcio Israel de Assunção Magalhães <lucioisraelti@gmail.com>
2022-02-02 00:04:06 -0300

    Meu primeiro ap androide

---
+++ src/main/java/br/com/treino/sorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package br.com.treino.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView texto = findViewById(R.id.textoExibicao);
+        int numero = new Random().nextInt(11);
+        texto.setText("O número selecionado é: " + numero);
+
+    }
+
+}
